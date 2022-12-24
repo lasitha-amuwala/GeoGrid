9acb178891d4f6cd16b2918d2dd792f355053c3f
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-12-24 16:37:08 -0500

    Seperated control panel into its own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,26 @@
-import Input from '../src/components/Input';
+import type { NextPage } from 'next';
+import { useCallback, useEffect, useState } from 'react';
+import { CgSpinner as Spinner } from 'react-icons/cg';
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+import { ControlPanel } from '../src/components/ControlPanel';
+import { createCordinateGrid } from '../src/components/utils/logic';
 import Map from '../src/components/Map';
 import Marker from '../src/components/Marker';
 
-import type { NextPage } from 'next';
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useCallback, useEffect, useState } from 'react';
-import { AutoComplete } from '../src/components/AutoComplete';
-import { ToggleColorMode } from '../src/components/ToggleColorMode';
-import { createCordinateGrid } from '../src/components/utils/logic';
-import { FaGithub } from 'react-icons/fa';
-import { TbGridDots } from 'react-icons/tb';
-import { RiPinDistanceFill } from 'react-icons/ri';
-import { BiKey } from 'react-icons/bi';
-import { MdExpandMore, MdExpandLess } from 'react-icons/md';
-import { CgSpinner as Spinner } from 'react-icons/cg';
-
 const Home: NextPage = () => {
-	const [isOpen, setOpen] = useState<boolean>(true);
 	const [gridSize, setGridSize] = useState<number>(3);
 	const [distance, setDistance] = useState<number>(100);
 	const [darkMode, setDarkMode] = useState<boolean>(true);
 	const [mapLoaded, setMapLoaded] = useState<Boolean>(false);
-	const [distanceText, setDistanceText] = useState<number>(100);
-	const [center, setCenter] = useState<google.maps.LatLngLiteral>({ lat: 43.653226, lng: -79.3831843 });
 	const [coordinateGrid, setCoordinateGrid] = useState<google.maps.LatLngLiteral[]>([]);
+	const [center, setCenter] = useState<google.maps.LatLngLiteral>({ lat: 43.653226, lng: -79.3831843 });
+
+	const toggleDarkMode = useCallback(() => setDarkMode(!darkMode), [darkMode]);
+	const onGridSizeChange = useCallback((size: number) => setGridSize(size), []);
+	const onDistanceChange = useCallback((dist: number) => setDistance(dist), []);
+	const onPlaceChange = (newCenter: google.maps.LatLngLiteral) => setCenter(newCenter);
 
 	useEffect(() => setCoordinateGrid(createCordinateGrid(center, gridSize, distance)), [center, gridSize, distance]);
-
-	const toggleIsOpen = useCallback(() => setOpen(!isOpen), [isOpen]);
-	const toggleDarkMode = useCallback(() => setDarkMode(!darkMode), [darkMode]);
-	const onPlaceChange = (newCenter: google.maps.LatLngLiteral) => setCenter(newCenter);
 
 	const LoadingComponent = () => (
 		<div className='w-full h-full justify-center items-center text-center bg-gray-800 flex flex-col gap-7'>
@@ -61,29 +51,6 @@ const Home: NextPage = () => {
 		);
 	};
 
-	const increaseGridSize = () => {
-		if (gridSize >= 15) return;
-		setGridSize((gridSize) => gridSize + 2);
-	};
-
-	const decreaseGridSize = () => {
-		if (gridSize <= 3) return;
-		setGridSize((gridSize) => gridSize - 2);
-	};
-
-	const increaseDistance = () => {
-		setDistance((distance) => distance + 50);
-	};
-
-	const decreaseDistance = () => {
-		if (distance - 50 < 50) return;
-		setDistance((distance) => distance - 50);
-	};
-
-	const onDistanceChange = (e: any) => {
-		setDistanceText(e.target.value);
-	};
-
 	return (
 		<div className={`${darkMode && 'dark'} h-[100dvh] relative overflow-hidden bg-gray-800`}>
 			<Wrapper
@@ -93,78 +60,15 @@ const Home: NextPage = () => {
 				render={mapRenderer}
 			/>
 			{mapLoaded && (
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ delay: 0.25 }}
-					className='h-auto w-auto sm:w-[450px] bg-white/[80%] dark:bg-gray-900/[80%] absolute bottom-0 left-0 right-0 backdrop-blur-xl rounded-2xl mx-3 my-7 sm:mx-10 px-5 py-5 shadow-2xl overflow-hidden'>
-					<div className='flex flex-col text-black dark:text-white'>
-						<div className='flex flex-row gap-3 items-center'>
-							<h2 className='font-bold text-4xl flex-grow'>GeoGrid</h2>
-							<button>
-								<a className='text-2xl'>
-									<FaGithub />
-								</a>
-							</button>
-							<ToggleColorMode darkMode={darkMode} toggle={toggleDarkMode} />
-							<button
-								onClick={toggleIsOpen}
-								className='rounded-full p-2 text-[22px] buttonColor transition-colors duration-200'>
-								{isOpen ? <MdExpandMore /> : <MdExpandLess />}
-							</button>
-						</div>
-						<AnimatePresence initial={false}>
-							{isOpen && (
-								<motion.div initial={{ height: 0 }} animate={{ height: 240 }} exit={{ height: 0 }}>
-									<div className='flex flex-col gap-5 overflow-hidden pt-5'>
-										<AutoComplete onPlaceChange={onPlaceChange} />
-										<Input icon={<BiKey />} placeholder='Enter a search keyword' />
-										<div className='flex gap-7'>
-											<div className='flex flex-col'>
-												<h2 className='flex-grow font-medium mb-2'>Grid Size</h2>
-												<div className='flex gap-1'>
-													<Input icon={<TbGridDots />} value={`${gridSize} x ${gridSize}`} readOnly />
-													<div className='flex flex-col gap-1'>
-														<button
-															className='px-[2px] rounded-t-md text-[18px] buttonColor transition-colors duration-200'
-															onClick={increaseGridSize}>
-															<MdExpandLess />
-														</button>
-														<button
-															className='px-[2px] rounded-b-md text-[18px] buttonColor transition-colors duration-200'
-															onClick={decreaseGridSize}>
-															<MdExpandMore />
-														</button>
-													</div>
-												</div>
-												<p className='text-xs text-gray-500 py-1'>Number of points</p>
-											</div>
-											<div className='flex flex-col'>
-												<h2 className='flex-grow font-medium mb-2'>Distance</h2>
-												<div className='flex gap-1'>
-													<Input icon={<RiPinDistanceFill />} value={distanceText} onChange={onDistanceChange} />
-													<div className='flex flex-col gap-1'>
-														<button
-															className='px-[2px] rounded-t-md text-[18px] buttonColor transition-colors duration-200'
-															onClick={increaseDistance}>
-															<MdExpandLess />
-														</button>
-														<button
-															className='px-[2px] rounded-b-md text-[18px] buttonColor transition-colors duration-200'
-															onClick={decreaseDistance}>
-															<MdExpandMore />
-														</button>
-													</div>
-												</div>
-												<p className='text-xs text-gray-500 py-1'>Search Distance (meters)</p>
-											</div>
-										</div>
-									</div>
-								</motion.div>
-							)}
-						</AnimatePresence>
-					</div>
-				</motion.div>
+				<ControlPanel
+					gridSize={gridSize}
+					distance={distance}
+					darkMode={darkMode}
+					toggleDarkMode={toggleDarkMode}
+					onPlaceChange={onPlaceChange}
+					onGridSizeChange={onGridSizeChange}
+					onDistanceChange={onDistanceChange}
+				/>
 			)}
 		</div>
 	);
---
+++ src/components/ControlPanel.tsx
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+import { AutoComplete } from './AutoComplete';
+import { ToggleColorMode } from './ToggleColorMode';
+import { AnimatePresence, motion } from 'framer-motion';
+import { BiKey } from 'react-icons/bi';
+import { FaGithub } from 'react-icons/fa';
+import { TbGridDots } from 'react-icons/tb';
+import { RiPinDistanceFill } from 'react-icons/ri';
+import { MdExpandLess, MdExpandMore } from 'react-icons/md';
+import Input from './Input';
+
+interface Props {
+	darkMode: boolean;
+	gridSize: number;
+	distance: number;
+	toggleDarkMode: () => void;
+	onPlaceChange: (args: google.maps.LatLngLiteral) => void;
+	onGridSizeChange: (arg: number) => void;
+	onDistanceChange: (arg: number) => void;
+}
+
+export const ControlPanel = ({
+	darkMode,
+	gridSize,
+	distance,
+	toggleDarkMode,
+	onPlaceChange,
+	onGridSizeChange,
+	onDistanceChange,
+}: Props) => {
+	const [isOpen, setOpen] = useState<boolean>(true);
+
+	const [distanceText, setDistanceText] = useState<number>(100);
+
+	const toggleIsOpen = () => setOpen(!isOpen);
+
+	const increaseGridSize = () => {
+		if (gridSize >= 15) return;
+		onGridSizeChange(gridSize + 2);
+	};
+
+	const decreaseGridSize = () => {
+		if (gridSize <= 3) return;
+		onGridSizeChange(gridSize - 2);
+	};
+
+	const increaseDistance = () => {
+		onDistanceChange(distance + 50);
+	};
+
+	const decreaseDistance = () => {
+		if (distance - 50 < 50) return;
+		onDistanceChange(distance - 50);
+	};
+
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ delay: 0.25 }}
+			className='h-auto w-auto sm:w-[450px] bg-white/[80%] dark:bg-gray-900/[80%] absolute bottom-0 left-0 right-0 backdrop-blur-xl rounded-2xl mx-3 my-7 sm:mx-10 px-5 py-5 shadow-2xl overflow-hidden'>
+			<div className='flex flex-col text-black dark:text-white'>
+				<div className='flex flex-row gap-3 items-center'>
+					<h2 className='font-bold text-4xl flex-grow'>GeoGrid</h2>
+					<button>
+						<a className='text-2xl'>
+							<FaGithub />
+						</a>
+					</button>
+					<ToggleColorMode darkMode={darkMode} toggle={toggleDarkMode} />
+					<button
+						onClick={toggleIsOpen}
+						className='rounded-full p-2 text-[22px] buttonColor transition-colors duration-200'>
+						{isOpen ? <MdExpandMore /> : <MdExpandLess />}
+					</button>
+				</div>
+				<AnimatePresence initial={false}>
+					{isOpen && (
+						<motion.div initial={{ height: 0 }} animate={{ height: 240 }} exit={{ height: 0 }}>
+							<div className='flex flex-col gap-5 overflow-hidden pt-5'>
+								<AutoComplete onPlaceChange={onPlaceChange} />
+								<Input icon={<BiKey />} placeholder='Enter a search keyword' />
+								<div className='flex gap-7'>
+									<div className='flex flex-col'>
+										<h2 className='flex-grow font-medium mb-2'>Grid Size</h2>
+										<div className='flex gap-1'>
+											<Input icon={<TbGridDots />} value={`${gridSize} x ${gridSize}`} readOnly />
+											<div className='flex flex-col gap-1'>
+												<button
+													className='px-[2px] rounded-t-md text-[18px] buttonColor transition-colors duration-200'
+													onClick={increaseGridSize}>
+													<MdExpandLess />
+												</button>
+												<button
+													className='px-[2px] rounded-b-md text-[18px] buttonColor transition-colors duration-200'
+													onClick={decreaseGridSize}>
+													<MdExpandMore />
+												</button>
+											</div>
+										</div>
+										<p className='text-xs text-gray-500 py-1'>Number of points</p>
+									</div>
+									<div className='flex flex-col'>
+										<h2 className='flex-grow font-medium mb-2'>Distance</h2>
+										<div className='flex gap-1'>
+											<Input icon={<RiPinDistanceFill />} value={distanceText} onChange={onDistanceChange} />
+											<div className='flex flex-col gap-1'>
+												<button
+													className='px-[2px] rounded-t-md text-[18px] buttonColor transition-colors duration-200'
+													onClick={increaseDistance}>
+													<MdExpandLess />
+												</button>
+												<button
+													className='px-[2px] rounded-b-md text-[18px] buttonColor transition-colors duration-200'
+													onClick={decreaseDistance}>
+													<MdExpandMore />
+												</button>
+											</div>
+										</div>
+										<p className='text-xs text-gray-500 py-1'>Search Distance (meters)</p>
+									</div>
+								</div>
+							</div>
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</div>
+		</motion.div>
+	);
+};
