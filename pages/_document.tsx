7434cb37f6dee54f5a0a9b0eb28498a13a3fce48
lasitha-amuwala <lasithaamuwala@gmail.com>
2022-12-24 04:53:38 -0500

    code formate

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
 	return (
 		<Html lang='en'>
-			<Head >
-				<meta name="theme-color" content="#111827" />
+			<Head>
+				<meta name='theme-color' content='#111827' />
 			</Head>
 			<body>
 				<Main />
