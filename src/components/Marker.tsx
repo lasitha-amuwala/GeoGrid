f8b274d055703ef92798ef6a4e4574d835980c3e
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-01-01 14:18:46 -0500

    Added nearby search service to marker

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -2,6 +2,20 @@ import { memo, useEffect, useState } from 'react';
 
 const Marker = memo((options: google.maps.MarkerOptions) => {
 	const [marker, setMarker] = useState<google.maps.Marker>();
+	const { map } = options;
+
+	var request = {
+		location: options.position as google.maps.LatLngLiteral,
+		radius: 500,
+		type: 'restaurant',
+	};
+
+	const service = new google.maps.places.PlacesService(map as google.maps.Map);
+	service.nearbySearch(request, (results, status) => {
+		if (status == google.maps.places.PlacesServiceStatus.OK && results) {
+			console.log('hi', results);
+		}
+	});
 
 	const circle = {
 		path: google.maps.SymbolPath.CIRCLE,
@@ -13,22 +27,15 @@ const Marker = memo((options: google.maps.MarkerOptions) => {
 	};
 
 	useEffect(() => {
-		if (!marker) {
-			setMarker(new google.maps.Marker());
-		}
-
+		if (!marker) setMarker(new google.maps.Marker());
 		// remove marker from map on unmount
 		return () => {
-			if (marker) {
-				marker.setMap(null);
-			}
+			if (marker) marker.setMap(null);
 		};
 	}, [marker]);
 
 	useEffect(() => {
-		if (marker) {
-			marker.setOptions({ label: '15', ...options });
-		}
+		if (marker) marker.setOptions({ label: '15', ...options });
 	}, [marker, options]);
 
 	return null;
