222a9af00f1cbcbbcbb22ce85aff912c48185115
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-01-13 23:05:01 -0500

    Added custom markers with overlayView class

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -1,45 +1,21 @@
-import { memo, useEffect, useState } from 'react';
+import OverlayView from './overlay/OverlayView';
 
-const Marker = memo((options: google.maps.MarkerOptions) => {
-	const [marker, setMarker] = useState<google.maps.Marker>();
-	const { map } = options;
+interface MarkerProps {
+	map?: google.maps.Map;
+	position: google.maps.LatLngLiteral;
+}
 
-	var request = {
-		location: options.position as google.maps.LatLngLiteral,
-		radius: 500,
-		type: 'restaurant',
-	};
-
-	const service = new google.maps.places.PlacesService(map as google.maps.Map);
-	service.nearbySearch(request, (results, status) => {
-		if (status == google.maps.places.PlacesServiceStatus.OK && results) {
-			console.log('hi', results);
-		}
-	});
-
-	const circle = {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: 'red',
-		fillOpacity: 0.6,
-		strokeWeight: 0,
-		rotation: 0,
-		scale: 17,
-	};
-
-	useEffect(() => {
-		if (!marker) setMarker(new google.maps.Marker());
-		// remove marker from map on unmount
-		return () => {
-			if (marker) marker.setMap(null);
-		};
-	}, [marker]);
-
-	useEffect(() => {
-		if (marker) marker.setOptions({ label: '15', ...options });
-	}, [marker, options]);
-
-	return null;
-});
-
-Marker.displayName = 'Marker';
-export default Marker;
+export const Marker = ({ map, position }: MarkerProps) => {
+	return (
+		<>
+			{map && (
+				<OverlayView position={position} map={map}>
+					<button>
+						<div
+							className={`bg-green-600 outline-1 outline text-black outline-black/40 w-10 h-10 rounded-full flex justify-center items-center text-lg`}>1</div>
+					</button>
+				</OverlayView>
+			)}
+		</>
+	);
+};
---
+++ src/components/overlay/OverlayView.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren, useEffect, useMemo } from 'react';
+import { createPortal } from 'react-dom';
+import { createOverlay } from './overlay';
+
+type OverlayProps = PropsWithChildren<{
+	position: google.maps.LatLng | google.maps.LatLngLiteral;
+	pane?: keyof google.maps.MapPanes;
+	map: google.maps.Map;
+	zIndex?: number;
+}>;
+
+export default function OverlayView({ position, pane = 'floatPane', map, zIndex, children }: OverlayProps) {
+	const container = useMemo(() => {
+		const div = document.createElement('div');
+		div.className = 'absolute';
+		div.style.translate = '-50% -50%';
+		return div;
+	}, []);
+
+	const overlay = useMemo(() => {
+		return createOverlay(container, pane, position);
+	}, [container, pane, position]);
+
+	useEffect(() => {
+		overlay?.setMap(map);
+		return () => overlay?.setMap(null);
+	}, [map, overlay]);
+
+	// to move the container to the foreground and background
+	useEffect(() => {
+		container.style.zIndex = `${zIndex}`;
+	}, [zIndex, container]);
+
+	return createPortal(children, container);
+}
