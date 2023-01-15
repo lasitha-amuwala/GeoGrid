9a88dded716e0ec8b73db72922dd4f16a8dc9e2b
lasitha-amuwala <lasithaamuwala@gmail.com>
2023-01-15 00:46:45 -0500

    Refactored home, removed document.tsx and added head and meta tags to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,16 @@ import { ControlPanel } from '../src/components/ControlPanel';
 import { createCordinateGrid } from '../src/utils/logic';
 import { GoogleMap } from '../src/components/map/GoogleMap';
 import { MarkerGridItem } from '../src/types/markers';
+import { useThemeContext } from './_app';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
 	const [gridSize, setGridSize] = useState<number>(3);
 	const [distance, setDistance] = useState<number>(100);
-	const [markerGrid, setMarkerGrid] = useState<MarkerGridItem[]>([]);
-	const [coordinateGrid, setCoordinateGrid] = useState<google.maps.LatLngLiteral[]>([]);
 	const [center, setCenter] = useState<google.maps.LatLngLiteral>({ lat: 43.653226, lng: -79.3831843 });
 	const [keyword, setKeyword] = useState<string>('');
-	const [searchResults, setSearchResults] = useState();
 
-	useEffect(() => setCoordinateGrid(createCordinateGrid(center, gridSize, distance)), [center, gridSize, distance]);
-	useEffect(() => {}, [coordinateGrid]);
-
-	useEffect(() => {
-		const markerGrid: MarkerGridItem[] = [];
-		coordinateGrid.map((coord) => {
-			markerGrid.push({ position: coord, rank: 0, results: [] });
-		});
-		setMarkerGrid(markerGrid);
-	}, [coordinateGrid]);
+	const { darkMode } = useThemeContext();
 
 	const onPlaceChange = (center: google.maps.LatLngLiteral) => setCenter(center);
 	const onGridSizeChange = useCallback((size: number) => setGridSize(size), []);
@@ -31,16 +21,28 @@ const Home: NextPage = () => {
 	const handleKeywordSubmit = useCallback((key: string) => setKeyword(key), []);
 
 	return (
-		<GoogleMap center={center} markers={markerGrid}>
-			<ControlPanel
-				gridSize={gridSize}
-				distance={distance}
-				onPlaceChange={onPlaceChange}
-				onGridSizeChange={onGridSizeChange}
-				onDistanceChange={onDistanceChange}
-				handleKeywordSubmit={handleKeywordSubmit}
-			/>
-		</GoogleMap>
+		<>
+			<Head>
+				<title>GeoGrid</title>
+				<meta name='description' content='Ranking google listing by keywords on a grid' />
+				<meta name='theme-color' content='#111827' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>
+				<div className={`${darkMode && 'dark'} h-[100dvh] relative overflow-hidden bg-gray-800`}>
+					<GoogleMap center={center} gridSize={gridSize} distance={distance} keyword={keyword}>
+						<ControlPanel
+							gridSize={gridSize}
+							distance={distance}
+							onPlaceChange={onPlaceChange}
+							onGridSizeChange={onGridSizeChange}
+							onDistanceChange={onDistanceChange}
+							handleKeywordSubmit={handleKeywordSubmit}
+						/>
+					</GoogleMap>
+				</div>
+			</main>
+		</>
 	);
 };
 
